352b5f8b1386747e97ff72438adcf771c4b82286
Rui-Araujo1409 <rui1409@gmail.com>
2026-02-25 16:26:35 +0000

    como obter os dados da promessa e inserir no html

--- script.js
+++ script.js
@@ -7,16 +7,19 @@ const submeterLocal = document.querySelector("#submeterLocal");
 submeterLocal.addEventListener("click", () => {
     const local = localInput.value;
     const urlAPI = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${local}?unitGroup=metric&key=GD3BYWX9NS5K7JR33T6S23368&contentType=json`
-    const fetchDadosTempo = async (url) => {
-        const resposta = await fetch(url);
-        const respostaObj = await resposta.json();
-        return respostaObj;
-    }
-
-    console.log(fetchDadosTempo(urlAPI));
+   fetchDadosTempo(urlAPI);
+   
 
 })
 
 
 
 
+ const fetchDadosTempo = async (url) => {
+        const resposta = await fetch(url);
+        const respostaObj = await resposta.json();
+        const respostaSumário = await respostaObj.description;
+        console.log(respostaSumário);
+        const textoSumário = document.querySelector(".sumario"); 
+        textoSumário.textContent = respostaSumário;
+    };
